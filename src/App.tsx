86757ab0a0c127c01c4a7df52e0a98862b08e90c
bhowmik1234 <124621278+bhowmik1234@users.noreply.github.com>
2024-07-16 21:38:12 +0530

    added auth

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import Loader from "./components/Loader";
 import Header from "./components/Header";
+import { Toaster } from "react-hot-toast";
 
 const Home = lazy(()=> import("./pages/Home"));
 const Cart = lazy(()=> import("./pages/Cart"));
@@ -87,6 +88,7 @@ export const App = () => {
           ;
         </Routes>
       </Suspense>
+      <Toaster position="bottom-left" />
     </Router>
   )
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,23 @@
 
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { useState } from "react";
+import toast from "react-hot-toast";
 import { FcGoogle } from "react-icons/fc";
+import { auth } from "../firebase";
 
 const Login = () => {
   const [gender, setGender] = useState("");
   const [date, setDate] = useState("");
 
-    const loginHandler =() =>{}
+    const loginHandler = async () =>{
+      try {
+        const provider = new GoogleAuthProvider();
+        const { user } = await signInWithPopup(auth, provider);
+        console.log(user);
+      } catch (error) {
+        toast.error('Sign in fail.');
+      }
+    }
 
   return (
     <div className="login">
